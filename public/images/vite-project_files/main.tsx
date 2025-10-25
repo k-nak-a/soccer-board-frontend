f811596cc7a10aa6e89f132f709f856259e5a2f5
kodai.nakajima <kodai.nakajima@persol.co.jp>
2025-10-25 17:40:51 +0900

    必要な画像が漏れていたのでcommit

---
+++ public/images/vite-project_files/main.tsx
@@ -0,0 +1,15 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=aa219386"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=aa219386"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=aa219386"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/Users/kodai.nakajima/Documents/git/soccer-board-frontend/src/main.tsx",
+  lineNumber: 5,
+  columnNumber: 5
+}, this) }, void 0, false, {
+  fileName: "/Users/kodai.nakajima/Documents/git/soccer-board-frontend/src/main.tsx",
+  lineNumber: 4,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBTUk7QUFOSixTQUFTQSxrQkFBa0I7QUFDM0IsU0FBU0Msa0JBQWtCO0FBQzNCLE9BQU9DLFNBQVM7QUFFaEJELFdBQVdFLFNBQVNDLGVBQWUsTUFBTSxDQUFFLEVBQUVDLE9BQzNDLHVCQUFDLGNBQ0MsaUNBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BQUksS0FETjtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BRUEsQ0FDRiIsIm5hbWVzIjpbIlN0cmljdE1vZGUiLCJjcmVhdGVSb290IiwiQXBwIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBTdHJpY3RNb2RlIH0gZnJvbSAncmVhY3QnXG5pbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAnXG5cbmNyZWF0ZVJvb3QoZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3Jvb3QnKSEpLnJlbmRlcihcbiAgPFN0cmljdE1vZGU+XG4gICAgPEFwcCAvPlxuICA8L1N0cmljdE1vZGU+LFxuKVxuIl0sImZpbGUiOiIvVXNlcnMva29kYWkubmFrYWppbWEvRG9jdW1lbnRzL2dpdC9zb2NjZXItYm9hcmQtZnJvbnRlbmQvc3JjL21haW4udHN4In0=
